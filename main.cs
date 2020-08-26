70dc0da13bb72b0891cbfbb95808d88042b02270
RenanCostaa <renancosta@ucl.br>
2020-08-26 02:48:28 +0000

    teste console.clear()

--- main.cs
+++ main.cs
@@ -1,6 +1,36 @@
 using System;
+/*
+3- Crie um programa que funcione para uma votação eleitoral. Existem três candidatos:
+33 - José Couve
+25 - Joana Bravo
+10 - Roberto Nove
+0 - Voto branco
+4 - Voto nulo
+
+Deseja-se saber:
+- O candidato vencedor;
+- O total de votos em branco;
+- O total de votos nulos;
+*/
 
 class MainClass {
+
+  public static void entraDados(){
+    Console.WriteLine("--- Lista de Candidatos ---");
+    Console.WriteLine("33 - José Couve");
+    Console.WriteLine("25 - Joana Bravo");
+    Console.WriteLine("10 - Roberto Nove");
+    Console.WriteLine("0  - Voto branco");
+    Console.WriteLine("4  - Voto nulo");
+    Console.ReadLine();
+    Console.Clear();
+    Console.ReadLine();
+  }
+  public static void votacao(){
+
+  }
+
+
   public static void Main () {
 
   }
